9285b91c1edb1af9b0c1054d7780673bb330bb28
Val Subaan <valsubaan@yahoo.com>
2025-10-01 11:59:03 +0800

    Added fallback to handoff_description, instructions, and api_key

--- mechanigo_agent.py
+++ mechanigo_agent.py
@@ -1,11 +1,10 @@
+from config import (
+    DEFAULT_AGENT_HANDOFF_DESCRIPTION,
+    DEFAULT_AGENT_INSTRUCTIONS
+)
 from agents import Agent, Runner
-from dotenv import load_dotenv
 import openai
 import logging
-import os
-
-load_dotenv()
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 logging.basicConfig(
     level=logging.INFO,
@@ -38,16 +37,17 @@ class MechaniGoAgent:
     def __init__(
         self,
         api_key: str = None,
-        name: str = None,
-        handoff_description: str = None,
-        instructions: str = None,
+        name: str = "MechaniGo Assistant",
         model: str = "gpt-4o-mini"
     ):
-        self.api_key = OPENAI_API_KEY if api_key is None else api_key
-        self.name = "MechaniGo Assistant" if name is None else name
+        self.api_key = api_key or openai.api_key or None
+        if not self.api_key:
+            raise ValueError("API key must be provided either directly or via environment variables.")
+
+        self.name = name
+        self.handoff_description = DEFAULT_AGENT_HANDOFF_DESCRIPTION
+        self.instructions = DEFAULT_AGENT_INSTRUCTIONS
         self.model = model
-        self.handoff_description = handoff_description
-        self.instructions = instructions
         self.agent = create_agent(
             self.api_key,
             self.name,
@@ -57,7 +57,7 @@ class MechaniGoAgent:
         )
         self.runner = RunnerWrapper
         self.logger = logging.getLogger(__name__)
-
+    
     async def inquire(self, inquiry: str):
         response = await self.runner.run(self.agent, inquiry)
         return response.final_output
